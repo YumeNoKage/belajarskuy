75d2f9695c6bdf0194c0a03323be1ca968439afb
vierynugroho <61929120+vierynugroho@users.noreply.github.com>
2021-11-21 10:45:15 +0700

    Update belajar.cpp

--- belajar.cpp
+++ belajar.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+#include <string.h>
+
+int main (){
+int menu, kopi, susu, teh, total;
+  
+  cout << "===== Menu Viery de Cafee =====" << endl;
+  cout << "-------------------------------" << endl;
+  cout << "========= Daftar Menu =========" << endl;
+  cout << " 1. Kopi " << endl;
+  cout << " 2. Susu " << endl;
+  cout << " 3. Teh " << endl;
+}
