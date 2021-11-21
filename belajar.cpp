3747df7c14a21585f25151d4c89fd1360bc99a86
vierynugroho <61929120+vierynugroho@users.noreply.github.com>
2021-11-21 10:55:07 +0700

    Update belajar.cpp

--- belajar.cpp
+++ belajar.cpp
@@ -2,7 +2,8 @@
 #include <string.h>
 
 int main (){
-int menu, kopi, susu, teh, total;
+int menu, pesanan, kopi, susu, teh, total;
+  
   
   cout << "===== Menu Viery de Cafee =====" << endl;
   cout << "-------------------------------" << endl;
@@ -10,4 +11,30 @@ int menu, kopi, susu, teh, total;
   cout << " 1. Kopi " << endl;
   cout << " 2. Susu " << endl;
   cout << " 3. Teh " << endl;
+  cout << "===============================" << endl;
+  
+  cout << "Masukkan Pesanan Anda = ";
+  cin >> pesanan;
+  if (pesanan == 1) {
+    cout << " Kopi Rp 5.000 " << endl;
+    harga = 5.000;
+  }
+  else if (pesanan == 2) {
+    cout << " Susu Rp 6.000 " << endl;
+    harga = 6.000;
+  }
+  else if (pesanan == 3) {
+    cout << " Teh Rp 4.000 " << endl;
+    harga = 4.000;
+  }
+  else {
+    cout << " Pesanan Tidak Tersedia " << endl;
+  }
+  
+  cout << " Masukkan Jumlah Pesanan Anda = ";
+  cin >> jumlah;
+  
+  cout << "------------------------------" << endl;
+  total = harga*jumlah;
+  cout << " Total Biaya Pesanan Anda Adalah = " << total << endl;
 }
